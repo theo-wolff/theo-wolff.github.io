8d2bee32b8f1a734a2c84b8e49fa45510dfb603c
theo-wolff <149272722+theo-wolff@users.noreply.github.com>
2023-10-30 23:50:43 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -14,27 +14,32 @@ navLinks.forEach(link => {
 })
 
 function changeImage() {
-    var img = document.getElementById('change-image');
+    var img = document.getElementById('image_school');
+    var title=document.getElementById('title_school');
+    var sub=document.getElementById('sub_school');
     switch(count){
         case 0: 
-            img.src = 'images/sidel.jpg';
+            img.src = 'images/looking.jpg';
+            title.innerText = 'Looking for an internship';
+            sub.innerText='I am currently looking for an internship starting in April 2023!';
             count++;
             break;
         case 1: 
-            img.src = 'images/CCpp.jpg';
+            img.src = 'images/revensburger.jpg';
+            title.innerText='Working at Ravensburger';
+            sub.innerText='During my summer holidays, I worked as a salesman/animator at Ravensburger';
             count++;
             break;
         case 2: 
-            img.src = 'images/foule.png';
-            count++;
-            break;
-
-        case 3:
-            img.src = 'images/robots.jpg';
+            img.src = 'images/web.jpg';
+            title.innerText='Web development';
+            sub.innerText='I am trying to learn web development on my own, starting with this portfolio!'
             count=0;
             break;
         default:
-            img.src = 'images/sidel.png';
+            img.src = 'images/looking.png';
+            title.innerText = 'Looking for and internship';
+            sub.innerText='I am currently looking for an internship starting in April 2023!';
             count=0;
     }
   }

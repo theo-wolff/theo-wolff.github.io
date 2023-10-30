d9d1e83fdc811b1c077239789186a393ae189582
theo-wolff <149272722+theo-wolff@users.noreply.github.com>
2023-10-30 09:38:19 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+
+var count = 1;
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,3 +12,29 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+function changeImage() {
+    var img = document.getElementById('change-image');
+    switch(count){
+        case 0: 
+            img.src = 'images/sidel.jpg';
+            count++;
+            break;
+        case 1: 
+            img.src = 'images/CCpp.jpg';
+            count++;
+            break;
+        case 2: 
+            img.src = 'images/foule.png';
+            count++;
+            break;
+
+        case 3:
+            img.src = 'images/robots.jpg';
+            count=0;
+            break;
+        default:
+            img.src = 'images/sidel.png';
+            count=0;
+    }
+  }
